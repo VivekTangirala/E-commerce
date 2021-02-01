ba03e1418fc24a127c28fb3baa21345f8a8c632c
aniteshreddy77 <aniteshreddy@protonmail.ch>
2021-02-01 10:30:27 +0530

    pull

--- lib/Cart/Cartapiremove.dart
+++ lib/Cart/Cartapiremove.dart
@@ -1,23 +1,19 @@
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'dart:convert';
 
 class Cartapiremove {
   static const uri = "http://infintymall.herokuapp.com/homepage/api/cart";
   static Future<Cartapiremove> removefromcart(String _productid) async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    String _token = sharedPreferences.getString("token");
+    String token = sharedPreferences.getString("token");
+    print(token);
     final baseurl = "http://infintymall.herokuapp.com/homepage/api/cart";
     final url = Uri.parse(baseurl);
-    Map data = {'product': _productid.toString()};
     final request = http.Request("DELETE", url);
     request.headers.addAll(<String, String>{
-      HttpHeaders.authorizationHeader: _token,
-      
+      HttpHeaders.authorizationHeader: token,  
     });
     request.body = jsonEncode({"product": _productid.toString()});
     final response = await request.send();
